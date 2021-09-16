aeda7baac49399f1bc1a8b3e6ca70519bc543eec
Filip <f.golub321@gmail.com>
2021-09-16 19:08:59 +0200

    Added IDs to gridElements, created ID list, added eventListeners

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,26 @@ const gridContainer=document.querySelector("#gridContainer");
 let gridElementsPerSide=16;
 let totalGridElements=Math.pow(gridElementsPerSide,2);
 console.log(totalGridElements);
+let idList=[];
 
-for(i=0;i<totalGridElements;i++){
+for(let i=0;i<totalGridElements;i++){
     const gridElement=document.createElement("div");
     gridElement.style.height="40px";
     gridElement.style.width="40px";
-    gridElement.style.backgroundColor="red";
+    gridElement.style.backgroundColor="coral";
+    let idOfGridElement="gridElement"+i;
+    idList.push(idOfGridElement);
+    gridElement.setAttribute("id", idOfGridElement);
     gridContainer.appendChild(gridElement);
 }
 
+console.log(idList);
 
+const gridElements=document.querySelectorAll("gridElement");
 
-
-
-
-const gridElements=document.querySelectorAll(".gridElement");
+gridElements.forEach((gridElement) => {
+    gridElement.addEventListener("hover", () => {
+        gridElement.style.backgroundColor="black";
+    })
+})
 
