92ab0184df1fe1a2fb54aeb3fae15cd4e3bdeee7
Filip <f.golub321@gmail.com>
2021-09-17 09:25:24 +0200

    Added functions to clear button to create a new custom grid

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,40 @@ const gridContainer=document.querySelector("#gridContainer");
 
 let gridElementsPerSide=16;
 let totalGridElements=Math.pow(gridElementsPerSide,2);
-console.log(totalGridElements);
+let gridElementSize=640/gridElementsPerSide;
 
-for(let i=0;i<totalGridElements;i++){
-    const gridElement=document.createElement("div");
-    gridElement.style.height="40px";
-    gridElement.style.width="40px";
-    gridElement.style.backgroundColor="coral";
-
-    gridElement.setAttribute("class", "gridClass");
-
-    gridElement.addEventListener("mouseover", () => {
-        gridElement.style.backgroundColor="black";
-    })
+function fillTheGrid(){
+    for(let i=0;i<totalGridElements;i++){
+        const gridElement=document.createElement("div");
+        gridElement.style.height=gridElementSize+"px";
+        gridElement.style.width=gridElementSize+"px";
+        gridElement.style.backgroundColor="coral";
     
-    gridContainer.appendChild(gridElement);
+        gridElement.setAttribute("class", "gridClass");
+    
+        gridElement.addEventListener("mouseover", () => {
+            gridElement.style.backgroundColor="black";
+        })
+        
+        gridContainer.appendChild(gridElement);
+    }
 }
 
 const clearButton=document.querySelector("#clearButton");
 
+function renewGrid(){
+    gridElementsPerSide=prompt("How many grid elements do you wish per side?");
+    totalGridElements=Math.pow(gridElementsPerSide,2);
+    gridElementSize=640/gridElementsPerSide;
+}
+
 clearButton.addEventListener("click", () => {
     document.querySelectorAll(".gridClass").forEach(e=>e.style.backgroundColor="coral");
+    renewGrid();
+    document.querySelectorAll(".gridClass").forEach(e=>e.remove());
+    fillTheGrid();
 }
 )
+
+
+fillTheGrid();
