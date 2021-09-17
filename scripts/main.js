50e0534c6867640e0e30db4f85ac43223e4eb8b9
Filip <f.golub321@gmail.com>
2021-09-17 08:47:03 +0200

    Added clear button which clears the etch-a-sketch. Deleted all ID related code. Added class.

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,25 @@ const gridContainer=document.querySelector("#gridContainer");
 let gridElementsPerSide=16;
 let totalGridElements=Math.pow(gridElementsPerSide,2);
 console.log(totalGridElements);
-let idList=[];
 
 for(let i=0;i<totalGridElements;i++){
     const gridElement=document.createElement("div");
     gridElement.style.height="40px";
     gridElement.style.width="40px";
     gridElement.style.backgroundColor="coral";
-    let idOfGridElement="gridElement"+i;
-    idList.push(idOfGridElement);
-    gridElement.setAttribute("id", idOfGridElement);
+
+    gridElement.setAttribute("class", "gridClass");
+
     gridElement.addEventListener("mouseover", () => {
         gridElement.style.backgroundColor="black";
     })
+    
     gridContainer.appendChild(gridElement);
 }
 
-console.log(idList);
-/*
-for(let i=0;i<totalGridElements;i++){
-    document.querySelector(idList[i]){
-        
-}
-*/
+const clearButton=document.querySelector("#clearButton");
 
+clearButton.addEventListener("click", () => {
+    document.querySelectorAll(".gridClass").forEach(e=>e.style.backgroundColor="coral");
+}
+)
